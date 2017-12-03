3d2be9cf230d34d291da722216ac73747854e46c
Anthony <a.penhard@gmail.com>
2017-12-03 14:31:23 +0100

    Php7.0 replace php5

--- install-trio.sh
+++ install-trio.sh
@@ -17,7 +17,7 @@
 # sudo sh ./install-trio.sh --with-mysql --login=knx --password=knx
 # sudo sh ./install-trio.sh --with-mysql --with-webmin
 #
-version="0.17"; # 12/03/2017
+version="0.18"; # 03/12/2017 => Php7.0
 
 if [ "$(id -u)" != "0" ]; then
    echo "     Attention!!!"
@@ -528,10 +528,10 @@ install_dependances ()
 
   if test "$_install_apache2" = yes;
   then
-    echo "Installation du serveur apache2 + Php5                  "
-    PAQUAGES=${PAQUAGES}" apache2 php5 libapache2-mod-php5 php5-common php5-cgi php5-fpm php5-cli php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-tidy php5-xmlrpc php5-xsl php5-json"
+    echo "Installation du serveur apache2 + Php7.0                  "
+    PAQUAGES=${PAQUAGES}" apache2 php7.0 libapache2-mod-php7.0 php7.0-common php7.0-cgi php7.0-fpm php7.0-cli php7.0-curl php7.0-gd php7.0-idn php-pear php7.0-imagick php7.0-imap php7.0-mcrypt php7.0-memcache php7.0-mhash php7.0-ming php7.0-ps php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-json"
     if test $_mysql_with = yes; then
-      PAQUAGES=${PAQUAGES}" php5-mysql"
+      PAQUAGES=${PAQUAGES}" php7.0-mysql"
     fi
     echo "-------------------------------------------------------------------"
     echo "Liste des paquets installés 5/6 : "
@@ -545,13 +545,13 @@ install_dependances ()
     echo " Apache2 deja installé "
     PHP_PATH=`which php`
     if test x$PHP_PATH = x; then :
-      PAQUAGES=${PAQUAGES}" php5 php5-common php5-cgi php5-cli php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-tidy php5-xmlrpc php5-xsl php5-json"
+      PAQUAGES=${PAQUAGES}" php7.0 php7.0-common php7.0-cgi php7.0-cli php7.0-curl php7.0-gd php7.0-idn php-pear php7.0-imagick php7.0-imap php7.0-mcrypt php7.0-memcache php7.0-mhash php7.0-ming php7.0-ps php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-json"
     else
       echo "-------------------------------------------------------------------"
-      echo "  PHP5 est installé "
+      echo "  PHP7.0 est installé "
     fi
     if test $_mysql_with = yes; then
-      PAQUAGES=${PAQUAGES}" php5-mysql"
+      PAQUAGES=${PAQUAGES}" php7.0-mysql"
     fi
     echo "-------------------------------------------------------------------"
     echo "Liste des paquets installés 6/6 : "
@@ -577,7 +577,7 @@ if test x$KNXD_PATH = x; then :
   echo " " > /var/log/knxd.log
   chmod 777 /var/log/knxd.log
 
-  apt-get install cdbs git-core build-essential debhelper cdbs autoconf automake libtool libusb-1.0-0-dev libsystemd-daemon-dev dh-systemd libev-dev --yes -y -qq
+  apt-get install git-core build-essential debhelper cdbs autoconf automake libtool libusb-1.0-0-dev libsystemd-dev dh-systemd libev-dev --yes -y -qq
   git clone https://github.com/knxd/knxd.git
 
   cd knxd
